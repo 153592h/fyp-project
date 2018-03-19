4c3e7ae167af0e72665bb34f69f8f2aee7f517ef
kai jun <153592h@mymail.nyp.edu.sg>
2018-03-19 15:48:36 +0800

    sdg

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -44,7 +44,7 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(699, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(758, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -71,37 +71,48 @@
             this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
             this.openFileToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
             this.openFileToolStripMenuItem.Text = "Open File...";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
             // 
             // saveFileToolStripMenuItem
             // 
+            this.saveFileToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            this.saveFileToolStripMenuItem.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.saveFileToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
             this.saveFileToolStripMenuItem.Text = "Save File...";
+            this.saveFileToolStripMenuItem.Click += new System.EventHandler(this.saveFileToolStripMenuItem_Click);
             // 
             // uploadToCozmoToolStripMenuItem
             // 
             this.uploadToCozmoToolStripMenuItem.Name = "uploadToCozmoToolStripMenuItem";
             this.uploadToCozmoToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
             this.uploadToCozmoToolStripMenuItem.Text = "Upload to Cozmo";
+            this.uploadToCozmoToolStripMenuItem.Click += new System.EventHandler(this.uploadToCozmoToolStripMenuItem_Click);
             // 
             // EditorTextBox
             // 
+            this.EditorTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.EditorTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
             this.EditorTextBox.Location = new System.Drawing.Point(12, 27);
             this.EditorTextBox.Multiline = true;
             this.EditorTextBox.Name = "EditorTextBox";
-            this.EditorTextBox.Size = new System.Drawing.Size(640, 369);
+            this.EditorTextBox.Size = new System.Drawing.Size(746, 401);
             this.EditorTextBox.TabIndex = 1;
+            this.EditorTextBox.TextChanged += new System.EventHandler(this.EditorTextBox_TextChanged);
             // 
             // EditorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(699, 440);
+            this.ClientSize = new System.Drawing.Size(758, 440);
             this.Controls.Add(this.EditorTextBox);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "EditorForm";
             this.Text = "Voice Assisted Programming";
+            this.Load += new System.EventHandler(this.EditorForm_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,55 @@ namespace WindowsFormsApplication1
         {
             EditorTextBox.Text = "";
         }
+
+        private void EditorTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svf = new SaveFileDialog();
+            svf.Filter = "Text Files(.txt)|*.txt";
+            svf.Title = "Save file...";
+            if (svf.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(svf.FileName);
+                sw.Write(EditorTextBox.Text);
+                sw.Close();
+            }
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Files(.txt)|*.txt";
+            ofd.Title = "Open a file...";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(ofd.FileName);
+                EditorTextBox.Text = sr.ReadToEnd();
+                sr.Close();
+
+            }
+        }
+
+        private void uploadToCozmoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // String cmr;
+            //cmr = EditorTextBox.Text;
+            //MessageBox.Show(cmr);
+            //System.Diagnostics.Process.Start("CMD.exe", EditorTextBox.Text);
+
+           PROCESS
+
+        }
+
 
         // public void RecognizeSpeech()
         // {
